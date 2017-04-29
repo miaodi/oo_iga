69efaad77308eb740f6f9f809a9da76dc6d631ee
miaodi <miaodi1987@gmail.com>
2017-04-29 14:47:17 -0400

    added some functions for knotvector

--- main.cpp
+++ main.cpp
@@ -17,8 +17,8 @@ int main() {
     b.InitClosed(1, 0, 1);
     Vector2d point1(0, 0);
     Vector2d point2(0, 1);
-    Vector2d point3(1, -1);
-    Vector2d point4(2, 2);
+    Vector2d point3(1, 0);
+    Vector2d point4(1, 1);
 
     vector<Vector2d> points({point1, point2, point3, point4});
     PhyTensorBsplineBasis<2, 2, double> domain(a, b, points);
@@ -31,6 +31,12 @@ int main() {
     cout<<domain.AffineMap(Vector2d(.5,.2))<<endl;
     domain.PrintKnots(0);
     domain.PrintKnots(1);
-    cout<<domain.Jacobian(Vector2d(.1,.2));
+    cout<<domain.Jacobian(Vector2d(.1,.2))<<endl;
+    auto x=PartialDerPattern<3>(2);
+    for(auto it = x->begin();it!=x->end();++it){
+        for(auto number:*it)
+            cout<<number<<" ";
+        cout<<endl;
+    }
     return 0;
 }
