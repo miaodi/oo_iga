6834d9726c83d82660e29d623db0df7d5a6c4540
miaodi <miaodi1987@gmail.com>
2017-09-23 17:37:21 -0600

    begin working on post processing.

--- PostProcess.h
+++ PostProcess.h
@@ -16,30 +16,42 @@ public:
     using CoordinatePairList = typename Element<T>::CoordinatePairList;
     using Quadlist = typename QuadratureRule<T>::QuadList;
     using Functor = std::function<std::vector<T>(const Coordinate &)>;
-    using ResultPtr = std::shared_ptr<PhyTensorBsplineBasis<2, 1, T>>;
+    using ResultPtr = std::pair<std::shared_ptr<Cell<T>>, std::shared_ptr<PhyTensorBsplineBasis<2, 1, T>>>;
 public:
-    PostProcess(const std::vector<std::shared_ptr<Cell<T>>> &dom, const std::vector<ResultPtr> &res,
-                const Functor &target) : _domains(dom), _results(res), _targetFunction(target) {};
+    PostProcess(const std::vector<std::shared_ptr<Cell<T>>> &dom,
+                const std::vector<std::shared_ptr<PhyTensorBsplineBasis<2, 1, T>>> &res,
+                const Functor &target) : _targetFunction(target) {
+        ASSERT(dom.size() == res.size(),
+               "The number of computational domains and that of result domains are not consistent.");
+        for (int i = 0; i < dom.size(); ++i) {
+            _results.push_back(std::make_pair(dom[i], res[i]));
+        }
+    };
 
     T RelativeL2Error() {
-        T error = 0;
-        for (const auto &domain:_domains) {
-            auto deg_x = domain->GetDegree(0);
-            auto deg_y = domain->GetDegree(1);
+        T errorNorm = 0;
+        T norm = 0;
+        for (const auto &result:_results) {
+            auto deg_x = result.first->GetDegree(0);
+            auto deg_y = result.first->GetDegree(1);
             QuadratureRule<T> quadrature;
             quadrature.SetUpQuadrature(deg_x >= deg_y ? (deg_x + 1) : (deg_y + 1));
             CoordinatePairList elements;
-            domain->KnotSpansGetter(elements);
-            for(const auto &element:elements){
+            result.first->KnotSpansGetter(elements);
+            for (const auto &element:elements) {
                 Quadlist quadratures;
                 quadrature.MapToQuadrature(element, quadratures);
-                
+                for (const auto &i:quadratures) {
+                    norm += i.second * pow(_targetFunction(result.first->GetDomain()->AffineMap(i.first))[0], 2);
+                    errorNorm += i.second * pow(_targetFunction(result.first->GetDomain()->AffineMap(i.first))[0] -
+                                                result.second->AffineMap(i.first)(0), 2);
+                }
             }
         }
+        return sqrt(errorNorm/norm);
     }
 
 private:
-    std::vector<std::shared_ptr<Cell<T>>> _domains;
     std::vector<ResultPtr> _results;
     Functor _targetFunction;
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <eigen3/Eigen/Dense>
 #include "PhyTensorBsplineBasis.h"
-#include "QuadratureRule.h"
 #include "Topology.h"
+#include "PostProcess.h"
 #include <fstream>
 #include <iomanip>
 #include <ctime>
@@ -18,114 +18,109 @@ using LoadFunctor = Element<double>::LoadFunctor;
 using Vector1d = Matrix<double, 1, 1>;
 
 int main() {
-    KnotVector<double> a;
-    a.InitClosed(1, 0, 1);
-    Vector2d point1(0, 0);
-    Vector2d point2(0, 2);
-    Vector2d point3(1, 0);
-    Vector2d point4(1, 2);
-    Vector2d point5(2, 0);
-    Vector2d point6(2, 2);
 
-    vector<Vector2d> points1({point1, point2, point3, point4});
-    vector<Vector2d> points2({point3, point4, point5, point6});
-    vector<Vector2d> points3({point1, point2});
+    int degreeElevate, refine;
+    for (degreeElevate = 1; degreeElevate < 6; degreeElevate++) {
+        for (refine = 1; refine < 6; refine++) {
 
-    auto domain1 = make_shared<PhyTensorBsplineBasis<2, 2, double>>(a, a, points1);
-    auto domain2 = make_shared<PhyTensorBsplineBasis<2, 2, double>>(a, a, points2);
-    domain1->DegreeElevate(3);
-    domain2->DegreeElevate(3);
+            KnotVector<double> a;
+            a.InitClosed(1, 0, 1);
+            Vector2d point1(0, 0);
+            Vector2d point2(0, 2);
+            Vector2d point3(2, -.5);
+            Vector2d point4(2, 2.5);
+            Vector2d point5(4, 0);
+            Vector2d point6(4, 2);
 
-    domain1->UniformRefine(2);
-    domain2->UniformRefine(2);
+            vector<Vector2d> points1({point1, point2, point3, point4});
+            vector<Vector2d> points2({point3, point4, point5, point6});
 
-    vector<shared_ptr<Cell<double>>> cells(2);
-    cells[0] = make_shared<Cell<double>>(domain1);
-    cells[1] = make_shared<Cell<double>>(domain2);
-    for (int i = 0; i < 1; i++) {
-        for (int j = i + 1; j < 2; j++)
-            cells[i]->Match(cells[j]);
-    }
-    for (int i = 0; i < 2; i++) {
-        cells[i]->PrintEdgeInfo();
-    }
+            auto domain1 = make_shared<PhyTensorBsplineBasis<2, 2, double>>(a, a, points1);
+            auto domain2 = make_shared<PhyTensorBsplineBasis<2, 2, double>>(a, a, points2);
 
+            domain1->DegreeElevate(degreeElevate);
+            domain2->DegreeElevate(degreeElevate);
+            domain1->KnotInsertion(1, 1.0 / 3);
+            domain1->KnotInsertion(1, 2.0 / 3);
+            domain1->UniformRefine(refine);
+            domain2->UniformRefine(refine + 1);
 
-    DofMapper<double> s;
-    BiharmonicMapperInitiator<double> visit(s);
-    for (int i = 0; i < 2; i++) {
-        cells[i]->accept(visit);
-    }
+            vector<shared_ptr<Cell<double>>> cells(2);
+            cells[0] = make_shared<Cell<double>>(domain1);
+            cells[1] = make_shared<Cell<double>>(domain2);
+            for (int i = 0; i < 1; i++) {
+                for (int j = i + 1; j < 2; j++)
+                    cells[i]->Match(cells[j]);
+            }
 
-    const double pi = 3.141592653589793238462643383279502884;
+            DofMapper<double> s;
+            BiharmonicMapperInitiator<double> visit(s);
+            for (int i = 0; i < 2; i++) {
+                cells[i]->accept(visit);
+            }
 
-    BiharmonicVisitor<double> biharmonic(s, [&pi](Coordinate u) -> vector<double> {
-        return vector<double>{4 * pow(pi, 4) * sin(pi * u(0)) * sin(pi * u(1))};
-    });
+            const double pi = 3.141592653589793238462643383279502884;
 
-    for (int i = 0; i < 2; i++) {
-        cells[i]->accept(biharmonic);
-    }
+            BiharmonicVisitor<double> biharmonic(s, [&pi](Coordinate u) -> vector<double> {
+                return vector<double>{4 * pow(pi, 4) * sin(pi * u(0)) * sin(pi * u(1))};
+            });
 
-    function<vector<double>(const Coordinate &)> Analytical = [&pi](const Coordinate &u) {
-        return vector<double>{sin(pi * u(0)) * sin(pi * u(1)), pi * cos(pi * u(0)) * sin(pi * u(1)),
-                              pi * sin(pi * u(0)) * cos(pi * u(1))};
-    };
-    BiharmonicBoundaryVisitor<double> boundary(s, Analytical);
+            for (int i = 0; i < 2; i++) {
+                cells[i]->accept(biharmonic);
+            }
 
-    for (int i = 0; i < 2; i++) {
-        cells[i]->accept(boundary);
-    }
+            function<vector<double>(const Coordinate &)> Analytical = [&pi](const Coordinate &u) {
+                return vector<double>{sin(pi * u(0)) * sin(pi * u(1)), pi * cos(pi * u(0)) * sin(pi * u(1)),
+                                      pi * sin(pi * u(0)) * cos(pi * u(1))};
+            };
+            BiharmonicBoundaryVisitor<double> boundary(s, Analytical);
 
-    BiharmonicInterfaceVisitor<double> interface(s);
+            for (int i = 0; i < 2; i++) {
+                cells[i]->accept(boundary);
+            }
 
-    for (int i = 0; i < 2; i++) {
-        cells[i]->accept(interface);
-    }
-    unique_ptr<SparseMatrix<double>> coupling = interface.Coupling();
+            BiharmonicBezierInterfaceVisitor<double> interface(s);
 
-    unique_ptr<SparseMatrix<double>> stiffness, load, boundaryValue;
-    tie(stiffness, load) = biharmonic.Domain();
-    boundaryValue = boundary.Boundary();
+            for (int i = 0; i < 2; i++) {
+                cells[i]->accept(interface);
+            }
+            unique_ptr<SparseMatrix<double>> coupling = interface.Coupling();
 
-    *stiffness = *coupling * (*stiffness) * coupling->transpose();
-    VectorXd loadSum = (*coupling * *load) - (*stiffness * *boundaryValue);
-    auto freedof = s.CondensedIndexMap();
+            unique_ptr<SparseMatrix<double>> stiffness, load, boundaryValue;
+            tie(stiffness, load) = biharmonic.Domain();
+            boundaryValue = boundary.Boundary();
 
-    VectorXd loadSol = *SparseTransform<double>(freedof, s.Dof()) * loadSum;
-    unique_ptr<SparseMatrix<double>> stiffnessSol = SparseMatrixGivenColRow<double>(freedof, freedof, stiffness);
+            *stiffness = *coupling * (*stiffness) * coupling->transpose();
+            VectorXd loadSum = (*coupling * *load) - (*stiffness * *boundaryValue);
+            auto freedof = s.CondensedIndexMap();
 
-    ConjugateGradient<Eigen::SparseMatrix<double>, Eigen::Lower | Eigen::Upper> cg;
-    cg.compute(*stiffnessSol);
-    VectorXd Solution = cg.solve(loadSol);
-    VectorXd boundaryDense = VectorXd(*boundaryValue);
-    VectorXd solution = coupling->transpose() * (SparseTransform<double>(freedof, s.Dof())->transpose() * Solution + boundaryDense);
+            VectorXd loadSol = *SparseTransform<double>(freedof, s.Dof()) * loadSum;
+            unique_ptr<SparseMatrix<double>> stiffnessSol = SparseMatrixGivenColRow<double>(freedof, freedof,
+                                                                                            stiffness);
 
-    vector<KnotVector<double>> solutionDomain1, solutionDomain2, solutionDomain3, solutionDomain4, solutionDomain5;
-    solutionDomain1.push_back(domain1->KnotVectorGetter(0));
-    solutionDomain1.push_back(domain1->KnotVectorGetter(1));
-    solutionDomain2.push_back(domain2->KnotVectorGetter(0));
-    solutionDomain2.push_back(domain2->KnotVectorGetter(1));
-    VectorXd controlDomain1 = solution.segment(s.StartingIndex(domain1), domain1->GetDof());
-    VectorXd controlDomain2 = solution.segment(s.StartingIndex(domain2), domain2->GetDof());
-    auto solution1 = PhyTensorBsplineBasis<2, 1, double>(solutionDomain1, controlDomain1);
-    auto solution2 = PhyTensorBsplineBasis<2, 1, double>(solutionDomain2, controlDomain2);
-    double x, y;
+            ConjugateGradient<Eigen::SparseMatrix<double>, Eigen::Lower | Eigen::Upper> cg;
+            cg.setMaxIterations(3 * stiffnessSol->cols());
+            cg.compute(*stiffnessSol);
+            VectorXd Solution = cg.solve(loadSol);
+            VectorXd boundaryDense = VectorXd(*boundaryValue);
+            VectorXd solution =
+                    coupling->transpose() *
+                    (SparseTransform<double>(freedof, s.Dof())->transpose() * Solution + boundaryDense);
 
-    ofstream file1, file2;
-    file1.open("domain1.txt");
-    file2.open("domain2.txt");
-    for (int i = 0; i <= 100; i++) {
-        for (int j = 0; j <= 100; j++) {
-            double xi = 1.0 * i / 100, eta = 1.0 * j / 100;
-            Vector2d u(xi, eta);
-            VectorXd position1 = domain1->AffineMap(u);
-            VectorXd position2 = domain2->AffineMap(u);
-            auto result1 = abs(solution1.AffineMap(u)(0) - Analytical(position1)[0]);
-            auto result2 = abs(solution2.AffineMap(u)(0) - Analytical(position2)[0]);
-            file1 << position1(0) << " " << position1(1) << " " << result1 << endl;
-            file2 << position2(0) << " " << position2(1) << " " << result2 << endl;
+            vector<KnotVector<double>> solutionDomain1, solutionDomain2, solutionDomain3, solutionDomain4, solutionDomain5;
+            solutionDomain1.push_back(domain1->KnotVectorGetter(0));
+            solutionDomain1.push_back(domain1->KnotVectorGetter(1));
+            solutionDomain2.push_back(domain2->KnotVectorGetter(0));
+            solutionDomain2.push_back(domain2->KnotVectorGetter(1));
+            VectorXd controlDomain1 = solution.segment(s.StartingIndex(domain1), domain1->GetDof());
+            VectorXd controlDomain2 = solution.segment(s.StartingIndex(domain2), domain2->GetDof());
+            vector<shared_ptr<PhyTensorBsplineBasis<2, 1, double>>> solutions(2);
+            solutions[0] = make_shared<PhyTensorBsplineBasis<2, 1, double>>(solutionDomain1, controlDomain1);
+            solutions[1] = make_shared<PhyTensorBsplineBasis<2, 1, double>>(solutionDomain2, controlDomain2);
+            PostProcess<double> post(cells, solutions, Analytical);
+            std::cout << post.RelativeL2Error() << std::endl;
         }
+        std::cout << std::endl;
     }
     return 0;
 }
